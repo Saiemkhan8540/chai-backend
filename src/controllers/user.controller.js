d5aef7c8db4fb88a8339c018fe04cadd383a6afb
Saiemkhan8540 <saiemkhan8540@gmail.com>
2026-04-16 15:47:35 +0530

    fixed refresh token validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -190,7 +190,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
          throw new ApiError(401, "Invalid refresh token")
      }
  
-     if (incomingRefreshToken !== user?.refreshToken){
+     if (!incomingRefreshToken !== user?.refreshToken){
          throw new ApiError(401, "Refresh Token is expired or used")
      }
  
